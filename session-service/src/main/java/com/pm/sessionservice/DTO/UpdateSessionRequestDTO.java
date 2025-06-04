4015b8a43350fa46d533a29be546685648d79acf
anieto1 <adnieto2019@gmail.com>
2025-06-04 16:24:26 -0400

    Created new DTO's for session microservice

--- session-service/src/main/java/com/pm/sessionservice/DTO/UpdateSessionRequestDTO.java
+++ session-service/src/main/java/com/pm/sessionservice/DTO/UpdateSessionRequestDTO.java
@@ -9,7 +9,6 @@ public class UpdateSessionRequestDTO {
     @Size(max = 30, message= "Title cannot exceed 30 characters")
     private String sessionName;
 
-    @Time
     private LocalDateTime scheduledTime;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
